fef0c98603030920436605463373aef3f8b28235
Balasivaguru <34646489+Balasivaguru@users.noreply.github.com>
2017-12-19 15:20:22 +0530

    Create variable.c

---
+++ beginner/variable.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+char name="Sachin Ramesh Tendulkar",nick="Little Master";
+int runs=18426,matches=463;
+float avrg=44.83;
+printf("%s\n%s\n%d\n%f\n%d",name,nick,runs,avrg,matches);
+}
